b05ad63f6f0f1ae28cd7a9aa3aaf14596125cebd
Nathan Krishnan <nathan.a.krishnan@gmail.com>
2022-06-21 17:53:16 -0700

    first commit: scaffold project

---
+++ src/taukReporter.ts
@@ -0,0 +1,52 @@
+import type { Reporters } from '@wdio/types';
+import WDIOReporter, {
+    RunnerStats, SuiteStats, TestStats,
+    BeforeCommandArgs, AfterCommandArgs, CommandArgs, Argument
+} from '@wdio/reporter';
+
+class TaukReporter extends WDIOReporter {
+
+    constructor(options: Partial<Reporters.Options>) {
+        options = Object.assign(options, { stdout: true });
+        super(options);
+    }
+
+    onRunnerStart(runnerStats: RunnerStats): void {
+        this.write(`[DEBUG] The runner has started! \n`);
+    }
+
+    onSuiteStart(suiteStats: SuiteStats): void {
+        this.write(`[DEBUG] The suite "${suiteStats.title}" has started! \n`);
+    }
+
+    onTestStart(testStats: TestStats): void {
+        this.write(`[DEBUG] The test "${testStats.title}" has started! \n`);
+    }
+
+    onTestPass(testStats: TestStats): void {
+        this.write(`[DEBUG] The test "${testStats.title}" has passed! \n`);
+    }
+
+    onTestFail(testStats: TestStats): void {
+        this.write(`[DEBUG] The test "${testStats.title}" has failed! \n`);
+    }
+
+    onTestSkip(testStats: TestStats): void {
+        this.write(`[DEBUG] The test "${testStats.title}" was skipped. \n`);
+    }
+
+    onTestEnd(testStats: TestStats): void {
+        this.write(`[DEBUG] The test "${testStats.title}" has ended. \n`);
+    }
+
+    onSuiteEnd(suiteStats: SuiteStats): void {
+        this.write(`[DEBUG] The suite "${suiteStats.title}" has ended! \n`);
+    }
+
+    onRunnerEnd(runnerStats: RunnerStats): void {
+         this.write(`[DEBUG] The runner has completed! \n`);
+    }
+
+}
+
+export default TaukReporter;
